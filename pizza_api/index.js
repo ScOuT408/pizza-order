3fd310a270ae211a7c48455023d961d3fd2ead6f
Biswajeet Dash <90475327+ScOuT408@users.noreply.github.com>
2023-03-16 21:49:26 +0530

    Updated index.js

--- pizza_api/index.js
+++ pizza_api/index.js
@@ -26,15 +26,16 @@ mongoose
     console.log(err.message);
   });
 
+
+app.use("/", (req, res) => {
+  res.send("Hello")
+});
+
 // http://localhost:5000/
 // routes
 app.use("/api/seed", seedRouter);
 app.use("/api/users", userRoute);
 app.use("/api/pizzas", productRoute);
-
-app.get("/", (req, res) => {
-  res.send("Hello")
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Running On ${process.env.PORT}`);
