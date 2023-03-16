bdaf2c14906db0b89e97a1de12ca0f235a27a9ff
Biswajeet Dash <90475327+ScOuT408@users.noreply.github.com>
2023-03-16 21:58:47 +0530

    Update index.js New

--- pizza_api/index.js
+++ pizza_api/index.js
@@ -12,9 +12,19 @@ dotenv.config();
 const app = express();
 
 // middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // db connection
 mongoose
