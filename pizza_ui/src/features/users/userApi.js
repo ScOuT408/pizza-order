6d5f2d242fd5decfb4af2e72c885cb98130839e9
Biswajeet Dash <90475327+ScOuT408@users.noreply.github.com>
2023-03-16 22:02:51 +0530

    Update userApi.js

--- pizza_ui/src/features/users/userApi.js
+++ pizza_ui/src/features/users/userApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const register = (formData) =>
-  axios.post("api/users/register", formData);
+  axios.post("https://api-pizza-five.vercel.app/api/users/register", formData);
 
-export const login = (formData) => axios.post("api/users/login", formData);
+export const login = (formData) =>
+  axios.post("https://api-pizza-five.vercel.app/api/users/login", formData);
+
