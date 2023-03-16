04fc763e96f1fd123c026268d16245aa45a87ea3
Biswajeet Dash <90475327+ScOuT408@users.noreply.github.com>
2023-03-16 22:02:26 +0530

    Update pizzaApi.js

--- pizza_ui/src/features/pizzas/pizzaApi.js
+++ pizza_ui/src/features/pizzas/pizzaApi.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-export const getPizzas = () => axios.get("api/pizzas/all");
+export const getPizzas = () => axios.get("https://api-pizza-five.vercel.app/api/pizzas/all");
 // export const getPizzaById = (id) => axios.get(`api/pizzas/${id}`);
